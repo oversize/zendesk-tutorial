dbd2a6b37fd601a63de34509f180b803c35fab5a
Manuel Schmidt <manuel.schmidt@vicampo.de>
2016-08-15 23:11:50 +0200

    Added handlebars templates

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,33 @@
+
+function showInfo() {
+  var requester_data = {
+    'name': 'Jane Doe',
+    'tags': ['tag1', 'tag2'],
+    'created_at': 'November 20, 2014',
+    'last_login_at': 'June 27, 2016'
+  };
+
+  var source = $("#requester-template").html();
+  var template = Handlebars.compile(source);
+  var html = template(requester_data);
+  $("#content").html(html);
+}
+
+
+function showError() {
+  var error_data = {
+    'status': 404,
+    'statusText': 'Not found'
+  };
+  var source = $("#error-template").html();
+  var template = Handlebars.compile(source);
+  var html = template(error_data);
+  $("#content").html(html);
+}
+
 $(function() {
   var client = ZAFClient.init();
   client.invoke('resize', { width: '100%', height: '400px' });
+  showInfo();
+  showError();
 });
